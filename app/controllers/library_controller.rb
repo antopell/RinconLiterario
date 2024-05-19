1a7ace27e9cb07b40538078e2b343c321673cf0e
moraagarcia <moragarcia2007@hotmail.com>
2024-05-19 16:35:40 -0300

    Agrego lógica para que se genere cada tarjeta de los libros consultados

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,19 +1,23 @@
 class LibraryController < ApplicationController
 
-    @books
+    def my_books
+        @books = @data ? @data['items'] : []
+    end
 
     def get_book
         api_service = ExternalApiService.new()
-        params = {search:"Coraline"}
+        params = {search:"Good Omens"}
         begin
           @data = api_service.fetch_books2(params)
         rescue => e
           @error = e.message
+          @data = nil
         end
     end
 
     def library
         get_book
+        my_books
     end
 
 end
