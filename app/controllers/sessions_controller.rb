851797616a6dff7a2b6a9417a98554ac4afd3cc6
moraagarcia <moragarcia2007@hotmail.com>
2024-05-23 20:47:53 -0300

    Agrego username en inicio y helpers para usuario actual loggeado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def crear
     @usuario = Usuario.find_by(mail: params[:mail])
     if (!!@usuario && @usuario.authenticate(params[:password]))
-      session[:usario_id] = @usuario.id
+      session[:usuario_id] = @usuario.id
       redirect_to root_path, notice: "Se inicio sesion correctamente"
     else
       redirect_to :logIn, alert: "No se pudo iniciar sesion"
