50bbf0cc6c98109df89a05b90bf32f5b75b83cd3
antopell <antopell29@gmail.com>
2024-05-23 17:44:02 -0300

    Arreglo estilos y saco codigo que no se usa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
     @usuario = Usuario.find_by(mail: params[:mail])
     if (!!@usuario && @usuario.authenticate(params[:password]))
       session[:usario_id] = @usuario.id
-      puts @usuario
       redirect_to root_path, notice: "Se inicio sesion correctamente"
     else
       redirect_to :logIn, alert: "No se pudo iniciar sesion"
