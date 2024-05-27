cd3fff86a5fe62c3ecada79a50d00bafed2ca3a2
moraagarcia <moragarcia2007@hotmail.com>
2024-05-27 19:45:21 -0300

    Agrego columna de reseñas en detalle de libros y también los esqueletos de los modales de los distintos botones de acción

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post 'logIn', to: "sessions#crear", as: "log_in"
   get 'library', to: 'library#library'
   get 'confirm_rating', to: 'book_details#confirm_rating', as: 'confirm_rating'
+  get 'create_rating', to: 'book_details#create_rating', as: 'create_rating'
+  get 'save_book', to: 'book_details#save_book', as: 'save_book'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
