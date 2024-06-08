1fe709431552e8c6460f72916fdc07d7bd3edf58
moraagarcia <moragarcia2007@hotmail.com>
2024-06-08 12:40:56 -0300

    Agrego funcionalidad de eliminar lectura y feedbacks de respuesta succes y danger

--- app/controllers/lecturas_controller.rb
+++ app/controllers/lecturas_controller.rb
@@ -29,14 +29,23 @@ class LecturasController < ApplicationController
         @lectura.book_title = session[:book_title]
         @book_img = session[:book_img]
         if (@lectura.save)
-            redirect_to book_detail_path(session[:book_id]), alert: "Se guardó la lectura exitosamente"
+            flash[:success] = "Se guardó la lectura exitosamente"
+            redirect_to library_path
         else 
-            redirect_to library_path, alert: "No se pudo guardar la lectura"
+            flash[:danger] = "No se pudo guardar la lectura"
+            redirect_to library_path
         end
     end
 
     def lectura_params
         params.require(:lectura).permit(:username, :book_id, :book_title, :book_img, :lecture_state, :reading_start_date, :reading_end_date)
+    end
+
+    def destroy
+        @lectura = Lectura.find(params[:id])
+        @lectura.destroy
+        flash[:danger] = "La lectura fue exitosamente eliminada"
+        redirect_to library_path
     end
     
 end
